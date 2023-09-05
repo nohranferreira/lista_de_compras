a5eb6d3a79631e5356b13fa8b8aa0f5a906edc09
nohranferreira <115414025+nohranferreira@users.noreply.github.com>
2023-09-05 19:41:05 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-const form = document.getElementById("novoItem");
-const lista = document.getElementById("lista");
-const itens = JSON.parse(localStorage.getItem("itens")) || [];
+import {
+	criaElemento,
+	atualizaElemento,
+	botaoDeleta,
+	deletaElemento,
+} from "./elementos.js";
+import { inicializaBusca } from "./busca.js";
+
+export const form = document.getElementById("novoItem");
+export const lista = document.getElementById("lista");
+export const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
 itens.forEach((elemento) => {
 	criaElemento(elemento);
@@ -41,76 +49,4 @@ form.addEventListener("submit", (evento) => {
 	quantidade.value = "";
 });
 
-const campoBusca = document.getElementById("campoBusca");
-
-campoBusca.addEventListener("input", function () {
-	const termoDeBusca = this.value.toLowerCase();
-
-	lista.childNodes.forEach((item) => {
-		const nomeItem = item.textContent.toLowerCase();
-		const estaVisivel = nomeItem.includes(termoDeBusca);
-
-		if (estaVisivel) {
-			item.classList.remove("hidden");
-		} else {
-			item.classList.add("hidden");
-		}
-	});
-});
-
-function criaElemento(item) {
-	const novoItem = document.createElement("li");
-	novoItem.classList.add("item");
-
-	const numeroItem = document.createElement("strong");
-	numeroItem.innerHTML = item.quantidade;
-	numeroItem.dataset.id = item.id;
-	novoItem.appendChild(numeroItem);
-
-	novoItem.innerHTML += item.nome;
-
-	const categoriaItem = document.createElement("span");
-	novoItem.classList.add("categoria");
-	categoriaItem.innerHTML = " [" + item.categoria + "]";
-	novoItem.appendChild(categoriaItem);
-
-	const checkbox = document.createElement("input");
-	checkbox.type = "checkbox";
-	checkbox.classList.add("item-checkbox");
-	novoItem.appendChild(checkbox);
-
-	novoItem.appendChild(botaoDeleta(item.id)); // Referenciar a função botaoDeleta no nó da função principal
-
-	lista.appendChild(novoItem);
-}
-
-function atualizaElemento(item) {
-	document.querySelector("[data-id='" + item.id + "']").innerHTML =
-		item.quantidade;
-}
-
-//Função para criar botão com evento de click nos itens, e retornar os itens clicados
-function botaoDeleta(id) {
-	const elementoBotao = document.createElement("button");
-	elementoBotao.classList.add("excluir");
-	elementoBotao.innerHTML = '<i class="fa fa-trash"></i>';
-
-	elementoBotao.addEventListener("click", function () {
-		deletaElemento(this.parentNode, id);
-	});
-
-	return elementoBotao;
-}
-
-//Função para deletar os itens enviados da função botaoDeleta no array de itens e no navegador
-
-function deletaElemento(tag, id) {
-	tag.remove();
-
-	itens.splice(
-		itens.findIndex((elemento) => elemento.id === id),
-		1
-	);
-
-	localStorage.setItem("itens", JSON.stringify(itens));
-}
+inicializaBusca();
